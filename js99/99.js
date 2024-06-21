eed2bb3c604141b77e8bb00a87d29e2fdd56a25d
いまはただかずま。 <167287371+imkzm2002@users.noreply.github.com>
2024-06-21 11:42:47 +0900

    Add files via upload

--- js99/99.js
+++ js99/99.js
@@ -1,10 +1,16 @@
-let html="";
-for(let x=1; x<=99; x+=1){
-    html +='<tr>';
-    for(let y=1; y<=99; y+=1){
-        html +='<td>'+ x*y +'</td>';
-    }
-    html += '</tr>'
+let html = "<tr><th></th>";
+for (let x = 1; x <= 99; x++) {
+    html += '<th>' + x + '</th>';
 }
+html += '</tr>';
+
+for (let y = 1; y <= 99; y++) {
+    html += '<tr><th>' + y + '</th>';
+    for (let x = 1; x <= 99; x++) {
+        html += '<td>' + x * y + '</td>';
+    }
+    html += '</tr>';
+}
+
 let elem = document.querySelector('table');
 elem.innerHTML = html;
